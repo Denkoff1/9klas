75a41c8172ac88d3e797ef3d7e203546c604f74c
Denkoff1 <157456408+Denkoff1@users.noreply.github.com>
2024-05-08 08:23:53 +0300

    Add files via upload

---
+++ 10chisla/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10chisla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] chisla = new int[10];
+            //Console.WriteLine("Въведи 10 числа!");
+            //for (int i = 0; i <10; i++)
+            //{
+            //    Console.Write("Въведи числo:");
+            //    chisla[i] = int.Parse(Console.ReadLine());
+
+            //}
+            //Console.WriteLine("Изход:");
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (chisla[i] % 2 == 0)
+            //    {
+            //        Console.WriteLine(chisla[i]);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Въведи четно числo!");
+            //    }
+            //}
+          
+            //Console.Write("Колко числа ще въвеждаш?:");
+            //int n=int.Parse(Console.ReadLine());
+            //int sum = 0;
+            //int[]chisla = new int[n];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write("Въведи число:");
+            //    chisla[i]=int.Parse(Console.ReadLine());
+            //}
+            //for(int i = 0;i < n; i++)
+            //{
+            //    sum += chisla[i]*i;
+                
+            //}
+            // Console.WriteLine(sum);
+
+        }
+    }
+}
